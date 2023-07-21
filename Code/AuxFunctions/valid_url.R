e47f5a3cbe5b7260866794c35bbf03327994ebe2
zchuri <22085722+zchuri@users.noreply.github.com>
2023-07-21 23:11:43 +0200

    Function to check URL

---
+++ Code/AuxFunctions/valid_url.R
@@ -0,0 +1,8 @@
+# Load function from StackoOver to check if a URL is valid
+# https://stackoverflow.com/a/60627969
+valid_url <- function(url_in,t=2){
+  con <- url(url_in)
+  check <- suppressWarnings(try(open.connection(con,open="rt",timeout=t),silent=T)[1])
+  suppressWarnings(try(close.connection(con),silent=T))
+  ifelse(is.null(check),TRUE,FALSE)
+}
